853b8b829fd9da1d3ad15736d46090c510c4f35a
Luqman Fauzi <luckman.fauzi@gmail.com>
2017-11-01 22:51:52 +0800

    Fix enzyme 3.0 dependency error

--- src/components/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as enzyme from 'enzyme';
-import App from './App';
-
-it('Renders without crashing', () => {
-  const title = enzyme.shallow(<App />);
-  expect(title.find('.Title').text()).toEqual('React HackerNews');
-});
